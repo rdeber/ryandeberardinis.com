cbed6d59c73078771fc370daeab1e7ec7ae8810e
Ryan DeBerardinis <ryan.deberardinis@learningpool.com>
2023-05-16 14:35:14 -0400

    Add framer motion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, styled, ThemeProvider } from '@mui/material/styles';
+import { motion } from "framer-motion"
 import "@fontsource/raleway/300.css";
 import "@fontsource/raleway/400.css";
 import "@fontsource/raleway/800.css";
@@ -93,8 +94,8 @@ const StyledFormContainer = styled(Paper)`
               0 15px 35px rgba(0,0,0,.1),
               0 50px 100px rgba(50,50,93,.1);
   will-change: transform;
-  transition: all .25s cubic-bezier(0,0,.2,1);
-  transform: perspective(3190px) rotateY(-27deg) rotateX(4deg) rotate(1deg);
+  // transition: all .25s cubic-bezier(0,0,.2,1);
+  // transform: perspective(3190px) rotateY(-27deg) rotateX(4deg) rotate(1deg);
 
   fieldset,
   textarea {
@@ -105,6 +106,31 @@ const StyledFormContainer = styled(Paper)`
 const StyledFormMessage = styled(TextField)`
   width: 100%
 `;
+
+const parentVariants = {
+  hidden: {
+    transition: {
+      staggerChildren: 0.1,
+      staggerDirection: -1
+    }
+  },
+  visible: {
+    transition: {
+      staggerChildren: 0.1
+    }
+  }
+};
+
+const childVariants = {
+  hidden: {
+    opacity: 0,
+    x: -16
+  },
+  visible: {
+    opacity: 1,
+    x: 0
+  }
+};
 
 export default function HomePage() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -135,66 +161,108 @@ export default function HomePage() {
           <span></span>
         </StyledBackground>
         <Grid item xs={12} sm={6} sx={{ p: 3 }}>
-          <Box>
-            <StyledH1 variant="h1">
+          <motion.div
+            variants={parentVariants}
+            initial="hidden"
+            animate={'visible'}
+         >
+            <StyledH1
+              component={motion.h1}
+              variants={childVariants}
+              variant="h1"
+            >
               <span>RYAN </span>
               <span>DeBERARDINIS</span>
             </StyledH1>
-            <StyledH2 variant="h2">
+            <StyledH2
+              component={motion.h2}
+              variants={childVariants}
+              variant="h2"
+            >
               Developer + Designer + Photographer
             </StyledH2>
-            <Typography variant="body1">
+            <Typography
+              component={motion.p}
+              variants={childVariants}
+              variant="body1"
+            >
               Experienced frontend developer and accessibility specialist with a proven track record of creating intuitive and accessible user experiences using the latest web technologies. Skilled in developing design systems that streamline workflows and improve user engagement, while ensuring compliance with WCAG standards. Passionate about driving innovation and solving complex design challenges through collaboration and user-centered design principles.
             </Typography>
-          </Box>
+          </motion.div>
         </Grid>
         <Grid item xs={12} sm={6} sx={{ p: 3 }}>
-          <StyledFormContainer>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="name"
-                label="Name"
-                name="name"
-                autoComplete="name"
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email"
-                name="email"
-                autoComplete="email"
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="phone"
-                label="Phone"
-                type="phone"
-                id="phone"
-                autoComplete="tel"
-              />
-              <StyledFormMessage
-                margin="normal"
-                id="message"
-                label="Message"
-                multiline
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Send Message
-              </Button>
-            </Box>
-          </StyledFormContainer>
+          <motion.div
+            initial={{ opacity: 0, transform: 'perspective(3000px) rotateY(0) rotateX(0) rotate(0)' }}
+            animate={{ opacity: 1, transform: 'perspective(3000px) rotateY(-27deg) rotateX(4deg) rotate(1deg)' }}
+            transition={{ duration: 0.5 }}
+          >
+            <StyledFormContainer>
+              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="name"
+                  label="Name"
+                  name="name"
+                  autoComplete="name"
+                  // @ts-expect-error
+                  component={motion.div}
+                  variants={childVariants}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  name="email"
+                  autoComplete="email"
+                  // @ts-expect-error
+                  component={motion.div}
+                  variants={childVariants}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="phone"
+                  label="Phone"
+                  type="phone"
+                  id="phone"
+                  autoComplete="tel"
+                  // @ts-expect-error
+                  component={motion.div}
+                  variants={childVariants}
+                />
+                <StyledFormMessage
+                  margin="normal"
+                  id="message"
+                  label="Message"
+                  multiline
+                  // @ts-expect-error
+                  component={motion.div}
+                  variants={childVariants}
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  // @ts-expect-error
+                  component={motion.button}
+                  variants={childVariants}
+                  // whileHover={{
+                  //   scale: 1.2,
+                  //   transition: { duration: 0.3 }
+                  // }}
+                  // whileTap={{ scale: 0.9 }}
+                >
+                  Send Message
+                </Button>
+              </Box>
+            </StyledFormContainer>
+          </motion.div>
         </Grid>
       </Grid>
     </ThemeProvider>
